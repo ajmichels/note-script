2b63598cfaaf3de0690a000292f53743e70cc1cf
AJ Michels <amichels@caringbridge.org>
2015-09-16 09:45:36 -0500

    Migrating bash script to PHP. Closes #4

--- src/Main.php
+++ src/Main.php
@@ -2,13 +2,47 @@
 
 namespace Ajmichels\NoteScript;
 
+use \Exception;
+
 class Main
 {
 
 
-    static public function run()
+    public static function run()
     {
-        echo 'hello world';
+
+        $noteDir = getenv('NOTE_HOME');
+
+        // Establish the directory where notes will be stored
+        if ($noteDir === false) {
+            $homeDir = getenv('HOME');
+
+            if ($homeDir !== false) {
+                $noteDir = $homeDir . '/notes';
+            } else {
+                throw new Exception('There is no $HOME environment variable defined.');
+            }
+
+        }
+
+        // Create the directory if it doesn't exist
+        if (!is_dir($noteDir)) {
+            mkdir($noteDir);
+        }
+
+        // Define the filename
+        $currentDate = date('Y-m-d h-i-s');
+        $fileName = str_replace(' ', '-', $currentDate) . '.md';
+        $filePath = $noteDir . '/' . $fileName;
+
+        // Create it if it doesn't exist
+        if (!file_exists($filePath)) {
+            file_put_contents($filePath, "# Note ${currentDate}\n\n");
+        }
+
+        // Output the path
+        echo $filePath;
+
     }
 
 
