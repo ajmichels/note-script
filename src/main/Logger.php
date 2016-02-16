7bd5a49cd286f33bf1598f126e28d6c5b937eb58
AJ Michels <ajmichels.web@gmail.com>
2016-02-15 21:26:54 -0600

    rename and re-namespace the project
    closes #14

--- src/main/Logger.php
+++ src/main/Logger.php
@@ -18,7 +18,7 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 
-namespace Ajmichels\NoteScript;
+namespace NoteScript;
 
 use Monolog\Logger as Monolog;
 use Monolog\Handler\SyslogHandler;
--- src/main/Main.php
+++ src/main/Main.php
@@ -18,7 +18,7 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 
-namespace Ajmichels\NoteScript;
+namespace NoteScript;
 
 use \Exception;
 use \ErrorException;
