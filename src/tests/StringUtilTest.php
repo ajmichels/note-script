133515b277822ff2dd94d7ac60b263547079c155
AJ Michels <ajmichels.web@gmail.com>
2016-12-21 22:42:34 -0600

    resolve long line of code

--- src/tests/StringUtilTest.php
+++ src/tests/StringUtilTest.php
@@ -43,7 +43,15 @@ class StringUtilTest extends TestCase
             ["Foo Bar", "foo-bar"],
             ["Foo Bar %$#@", "foo-bar"],
             ["Foo      Bar", "foo-bar"],
-            ["Lorem ipsum dolor sit amet, consectetur adipiscing elit. Illa videamus, quae a te de amicitia dicta sunt. Non enim iam stirpis bonum quaeret, sed animalis. Claudii libidini, qui tum erat summo ne imperio, dederetur. Recte, inquit, intellegis. Duo Reges: constructio interrete.", "lorem-ipsum-dolor-sit-amet-consectetur-adipiscing-elit-illa-videamus-quae-a-te-de-amicitia-dicta-sunt-non-enim-iam-stirpis-bonum-quaeret-sed-animalis-claudii-libidini-qui-tum-erat-summo-ne-imperio-dederetur-recte-inquit-intellegis-duo"],
+            [
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Illa videamus, quae a " .
+                "te de amicitia dicta sunt. Non enim iam stirpis bonum quaeret, sed animalis. " .
+                "Claudii libidini, qui tum erat summo ne imperio, dederetur. Recte, inquit, ".
+                "intellegis. Duo Reges: constructio interrete.",
+                "lorem-ipsum-dolor-sit-amet-consectetur-adipiscing-elit-illa-videamus-quae-a-te-" .
+                "de-amicitia-dicta-sunt-non-enim-iam-stirpis-bonum-quaeret-sed-animalis-claudii-" .
+                "libidini-qui-tum-erat-summo-ne-imperio-dederetur-recte-inquit-intellegis-duo"
+            ],
         ];
     }
 
